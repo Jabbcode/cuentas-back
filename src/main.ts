fc75fdbc24d948f3b776dfe25bb46db21cd0d348
Jabbcode <jabbsontouch@gmail.com>
2024-09-23 22:38:09 +0200

    feat: Agregando CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const cortsOptions: CorsOptions = {
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 200,
+  }
+
+  app.enableCors(cortsOptions)
 
   app.setGlobalPrefix('api/v1')
   await app.listen(3000)
